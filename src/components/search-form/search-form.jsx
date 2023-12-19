f94bbe908ac136b42166588a2de8317eecc282a1
Dima Os <dimaos@proton.me>
2023-12-19 19:39:13 +0300

    Реализует поиск

--- src/components/search-form/search-form.jsx
+++ src/components/search-form/search-form.jsx
@@ -1,11 +1,25 @@
+import {  useNavigate } from 'react-router-dom';
+
 import styles from "./search-form.module.scss"
 import Input from '../input/input';
 import Button from '../button/button';
 
 export default function SearchForm() {
+  const navigate = useNavigate();
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    let searchQuery = evt.target.search.value;
+
+    if (searchQuery) {
+      navigate(`/search?s=${searchQuery}`);
+      evt.target.search.value = '';
+    };
+  }
 
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <Input
         classes={styles.input}
         type="search"
@@ -15,7 +29,7 @@ export default function SearchForm() {
         label="Search cocktail"
         labelClasses="visually-hidden"
       />
-      <Button classes={styles.button} type="button">Search</Button>
+      <Button classes={styles.button} type="submit">Search</Button>
     </form>
   );
 };
--- src/pages/search-page/search-page.jsx
+++ src/pages/search-page/search-page.jsx
@@ -1,13 +1,53 @@
+import { useState, useEffect, useCallback } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { ACCESS_URL } from '../../utils/constants';
+
 import styles from './search-page.module.scss'
+import SearchBar from '../../components/search-bar/search-bar'
+import Loader from '../../components/loader/loader';
+import CocktailCard from '../../components/cocktail-card/cocktail-card';
+
+function Results({ cocktails }) {
+
+
+  return (
+    cocktails
+      ? <>
+          <h1 className={styles.title}>Search results</h1>
+          <ul className={styles.list}>
+            {cocktails.map(item => (
+              <li key={item.idDrink}  className={styles.item}>
+                <CocktailCard cocktail={item} />
+              </li>
+            ))}
+          </ul>
+        </>
+      : <h1 className={styles.title}>Nothing was found</h1>
+  );
+};
 
 export default function SearchPage() {
+  const location = useLocation();
+  const [cocktails, setCocktails] = useState(null);
+  const [isResponseReceived, setIsResponseReceived] = useState(false);
+
+  const aaa = useCallback(setIsResponseReceived);
+
+  useEffect(() => {
+    fetch(`${ACCESS_URL}/search.php${location.search}`)
+      .then(res => res.json())
+      .then(data => {
+        setCocktails(data.drinks);
+        setIsResponseReceived(true);
+      });
+  }, [location]);
 
   return (
     <section className={styles.section}>
       <div className="container">
-        <div className={styles.inner}>
-          <h1>Search page</h1>
-        </div>
+        <SearchBar />
+        {isResponseReceived ? <Results cocktails={cocktails} /> : <Loader />}
       </div>
     </section>
   );
